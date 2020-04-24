5a124b4c987604394cfd969b0dc654c8e017aba6
za-wanggang <wanggang@nuanwa.net>
2020-04-24 11:56:57 +0800

    update x86config

--- diy.sh
+++ diy.sh
@@ -6,4 +6,46 @@
 # Author: LinSir
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.1.248/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.252/g' package/base-files/files/bin/config_generate
+
+
+echo '修改机器名称'
+sed -i 's/OpenWrt/CeylogWrt/g' package/base-files/files/bin/config_generate
+
+echo '修改时区'
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+# echo '修改默认主题'
+# sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
+
+# echo '去除默认bootstrap主题'
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+
+# Modify the version number
+sed -i "s/OpenWrt /Ceylog build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+echo '删除旧版argon,链接新版'
+rm -rf ./package/lean/luci-theme-argon
+ln -s ../../../luci-theme-argon ./package/lean/
+
+
+echo '修改banner'
+rm -rf package/base-files/files/etc/banner
+cp -f ../banner package/base-files/files/etc/
+
+echo '下载ServerChan'
+git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
+
+echo '集成diy目录'
+ln -s ../../diy ./package/openwrt-packages
+
+# Add kernel build user
+[ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_USER="Ceylog"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"Ceylog"@' .config
+
+# Add kernel build domain
+[ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_DOMAIN="wglgg.cn"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"wglgg.cn"@' .config
